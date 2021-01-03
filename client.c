63c07f0feced492d2b703c2520b6f8483c4bb0a2
yen0515 <g0920410515@gmail.com>
2021-01-03 17:03:30 +0800

    :raccoon: update

--- client.c
+++ client.c
@@ -1,24 +1,74 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define SHM_KEY 123456
+#define SHMSZ 17
+ 
+int main()
+{
+    int shmid = 0, i = 0;
+    key_t key;
+    char *shm = NULL;
+    int ptr[(SHMSZ - 1)];
+    
+    // Segment key.
+    
+    key = SHM_KEY;
+    
+    // Create the segment.
+    
+    if( ( shmid = shmget( key, SHMSZ, IPC_CREAT | 0666 ) ) < 0 )
+    {
+        perror( "shmget" );
+        exit(1);
+    }
+    
+    // Attach the segment to the data space.
+    
+    if( ( shm = shmat( shmid, NULL, 0 ) ) == (char *)-1 )
+    {
+        perror( "shmat" );
+        exit(1);
+    }
+    
+    //memset( shm, 0, SHMSZ );
 
-int *ptr;
+    // Change the shared memory content.
+    // *shm == 0, wait for change.
+    // *shm == 1, not processed yet.
+    // *shm == 2, bye.
+    
+    while(1)
+    {
+        int cmd;
+        printf("\n");
+        printf("1: Show the value\n");
+        printf("2: Modify the value\n");
+        printf("3: Exit\n");
+        printf("Enter commands: ");
+        scanf("%d", &cmd);
 
-int main () {
-    int id;
-
-    /************************* TODO 1 *************************/
-    // Attach the memory section, the return value is a pointer to 
-    // the shared memory section
-    /************************* TODO 1 *************************/
-
-    printf("\033[1;33m[client] The value is %d\033[0m\n", ptr[0]);
-
-    while(1) {
+        if (cmd == 1)
+            printf("\033[1;32m[client] The value is %d\033[0m\n", *shm);
+        else if (cmd == 2) {
+            printf("Input new value: ");
+            memset( ptr, 0, ( SHMSZ - 1 ) );
+            //fgets( ptr, ( SHMSZ - 1 ), stdin );
+            scanf("%d", ptr);
+            memcpy(shm, ptr, ( SHMSZ - 1 ));
+        }
+        else
+            break;          
+        sleep(1);
+    }
+    
+    return 0;
+} 
+/*while(1) {
         int cmd;
 
         printf("\n");
@@ -29,12 +79,11 @@ int main () {
         scanf("%d", &cmd);
 
         if (cmd == 1)
-            printf("\033[1;33m[client] The value is %d\033[0m\n", ptr[0]);
+            printf("\033[1;32m[server] The value is %d\033[0m\n", ptr[0]);
         else if (cmd == 2) {
             printf("Input new value: ");
             scanf("%d", &ptr[0]);
         }
         else
             break;        
-    } 
-}
+    } */
